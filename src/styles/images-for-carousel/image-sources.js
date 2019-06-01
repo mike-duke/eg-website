44658510c5c0fd96ef0348f2f28e47ef5bd736a8
Mike Duke <aithon@gmail.com>
2019-06-01 13:34:22 -0600

    Add Nuka Carousel and have it working on the page.

---
+++ src/styles/images-for-carousel/image-sources.js
@@ -0,0 +1,12 @@
+export const images = [
+  {id: 1, src: 'https://www.enchantedgrounds.com/sites/default/files/styles/large/public/11.jpg', alt: 'the coffee counter at Highlands Ranch'},
+  {id: 2, src: 'https://www.enchantedgrounds.com/sites/default/files/styles/large/public/14_0.jpg', alt: 'a display case full of magic the gathering cards at Highlands Ranch'},
+  {id: 3, src: 'https://www.enchantedgrounds.com/sites/default/files/styles/large/public/magic-draft-3.jpg', alt: 'players at a magic the gathering draft event at Highlands Ranch'},
+  {id: 4, src: 'https://www.enchantedgrounds.com/sites/default/files/styles/large/public/magic-draft-7.jpg', alt: 'players at a magic the gathering draft event at Highlands Ranch'},
+  {id: 5, src: 'https://www.enchantedgrounds.com/sites/default/files/styles/large/public/13.jpg', alt: 'the wall of magic the gathering products at Highlands Ranch'},
+  {id: 6, src: 'https://www.enchantedgrounds.com/sites/default/files/styles/large/public/10.jpg', alt: 'the store front at Enchanted Grounds in Highlands Ranch'},
+  {id: 7, src: 'https://www.enchantedgrounds.com/sites/default/files/styles/large/public/16.jpg', alt: 'the coffee area seating at Highlands Ranch'},
+  {id: 8, src: 'https://www.enchantedgrounds.com/sites/default/files/styles/large/public/12.jpg', alt: 'a view into the full store at Highlands Ranch'},
+  {id: 9, src: 'https://www.enchantedgrounds.com/sites/default/files/styles/large/public/40K.jpg', alt: 'playing Warhammer 40K at Highlands Ranch'},
+  {id: 10, src: 'https://www.enchantedgrounds.com/sites/default/files/styles/large/public/40K-1v1.jpg', alt: 'playing Warhammer 40K at Highlands Ranch'}
+];
